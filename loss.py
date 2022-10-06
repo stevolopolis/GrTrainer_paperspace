cba9d56bfdc2404ec14f3848fe2ef8ed60b0a85d
stevolopolis <stevenluots@gmail.com>
2022-10-06 13:52:48 -0400

    deleted unused code and functions.

--- loss.py
+++ loss.py
@@ -12,11 +12,7 @@ def MapLoss(output, map):
 
     # Best for CLS
     confidence_loss = nll_loss(output[:, :, :, 5], map[:, :, :, 5])
-    #confidence_loss = torch.pow(output[:, :, :, 5] - map[:, :, :, 5], 2)
-    #confidence_loss = torch.abs(output[:, :, :, 5] - map[:, :, :, 5])
     confidence_loss = torch.mean(confidence_loss)
-    # L2 loss (grasp)
-    #bbox_loss = torch.pow(output[:, :, :, :5] - map[:, :, :, :5], 2)
     # L1 loss (grasp)
     bbox_loss = torch.abs(output[:, :, :, :5] - map[:, :, :, :5])
     # Cross Entropy Loss (cls)
@@ -51,47 +47,6 @@ def minLossTarget(output, target_candidates):
             min_loss_batch = torch.cat((min_loss_batch, min_target), dim=0)
 
     return min_loss_batch
-
-
-def BoundingLoss(output, target):
-    """Returns L1 distance if output is counted as a valid grasp,
-       and returns higher loss if not."""
-    pass
-
-
-def CLSLoss(output, target):
-    """l1_loss = F.l1_loss(output, target)
-    output_sum = torch.sum(output, dim=1)
-    sum_diff = torch.abs(1 - output_sum)
-    sum_cond = sum_diff < 1
-    sum_loss = - torch.log(torch.where(sum_cond, sum_diff, torch.ones_like(sum_diff, dtype=torch.float32)))
-    loss = l1_loss + sum_loss"""
-    loss = nll_loss(output, target)
-
-    return torch.sum(loss) / loss.size(0)
-
-
-def WeightedL2Loss(output, target):
-    mse = torch.pow(output - target, 2)
-    cond = [1, 1, 0, 0, 0]
-    loss = torch.where(cond, 2*mse, mse)
-
-    return torch.sum(loss) / loss.size(0)
-
-
-def BCEL1Loss(output, target):
-    """Returns BCELoss for when output is in [0, 1], and
-    returns L1Loss for when output is not in [0, 1].
-
-    Implemented using the format of smooth_l1_loss.
-    """
-    smaller_than_one = output < .9
-    greater_than_zero = output > .1
-
-    condition = smaller_than_one == greater_than_zero
-    loss = torch.where(condition, nll_loss(output, target), torch.abs(output - target) + .1)
-    
-    return torch.sum(loss) / loss.size(0)
 
 
 def nll_loss(output, target):
