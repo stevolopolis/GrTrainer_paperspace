87ac6d41bd8925266cc4fd6fd23f5a20241199d9
stevolopolis <stevenluots@gmail.com>
2022-10-27 10:26:21 -0400

    modified for new dataset retrieved from server.

--- test.py
+++ test.py
@@ -27,26 +27,26 @@ import torch
 import os
 
 from parameters import Params
-import inference.models.alexnet_old as models
+import inference.models.alexnet as models
 from evaluation import get_cls_acc, get_grasp_acc, visualize_grasp, visualize_cls
 
 params = Params()
 
 model_name = params.MODEL_NAME
 weights_dir = params.MODEL_PATH
-for epoch in range(100, 101):
+for epoch in range(150, 151):
     weights_path = os.path.join(weights_dir, model_name, model_name + '_epoch%s.pth' % epoch)
 
     # AlexNet with 1st, 2nd layer pretrained on Imagenet
-    model = models.AlexnetMap_v5(n_cls=params.NUM_CLASS).to(params.DEVICE)
+    model = models.AlexnetMap().to(params.DEVICE)
     model.load_state_dict(torch.load(weights_path))
     model.eval()
     # Get test acc for CLS model
     #accuracy, loss = get_cls_acc(model)
     # Get test acc for Grasp model
-    accuracy, loss = get_grasp_acc(model)
+    #accuracy, loss = get_grasp_acc(model)
 
-    print('Epoch: %s' % epoch, accuracy, loss)
+    #print('Epoch: %s' % epoch, accuracy, loss)
     
     # Visualize CLS predictions one by one
     #visualize_cls(model)
