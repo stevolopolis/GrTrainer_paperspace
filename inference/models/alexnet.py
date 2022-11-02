6dc169251a63ed4d40a7ba49c5d1f8b0d704c18a
stevolopolis <stevenluots@gmail.com>
2022-11-02 10:17:27 -0400

    added new experimental architecture for training.

--- inference/models/alexnet.py
+++ inference/models/alexnet.py
@@ -8,9 +8,29 @@ outputs a map of predictions.
 """
 
 import torch
+import torch.nn.functional as F
 import torch.nn as nn
 
 from torchvision.models import alexnet
+
+
+class ResidualBlock(nn.Module):
+    """
+    A residual block with dropout option
+    """
+
+    def __init__(self, in_channels, out_channels, kernel_size=3):
+        super(ResidualBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size, padding=1)
+        self.bn1 = nn.BatchNorm2d(in_channels)
+        self.conv2 = nn.Conv2d(in_channels, out_channels, kernel_size, padding=1)
+        self.bn2 = nn.BatchNorm2d(in_channels)
+
+    def forward(self, x_in):
+        x = self.bn1(self.conv1(x_in))
+        x = F.relu(x)
+        x = self.bn2(self.conv2(x))
+        return x + x_in
 
 
 class AlexnetMap(nn.Module):
@@ -95,7 +115,146 @@ class AlexnetMap_v2(nn.Module):
 
         self.confidence = nn.Sequential(
             nn.ConvTranspose2d(32, 1, kernel_size=11, stride=4, output_padding=1),
+            nn.Sigmoid()
+        )
+
+        for param in self.rgb_features.parameters():
+            param.requires_grad = False
+        for param in self.d_features.parameters():
+            param.requires_grad = False
+
+        # xavier initialization for combined feature extractor
+        for m in self.features.modules():
+            if isinstance(m, (nn.Conv2d, nn.ConvTranspose2d, nn.Linear)):
+                nn.init.xavier_uniform_(m.weight, gain=1)
+
+    def forward(self, x):
+        rgb = x[:, :3, :, :]
+        d = torch.unsqueeze(x[:, 3, :, :], dim=1)
+        d = torch.cat((d, d, d), dim=1)
+
+        rgb = self.rgb_features(rgb)
+        d = self.d_features(d)
+        x = torch.cat((rgb, d), dim=1)
+
+        x = self.features(x)
+        grasp = self.grasp(x)
+        confidence = self.confidence(x)
+        out = torch.cat((grasp, confidence), dim=1)
+
+        return out
+
+    # Unfreeze pretrained layers (1st & 2nd CNN layer)
+    def unfreeze_depth_backbone(self):
+        for param in self.rgb_features.parameters():
+            param.requires_grad = True
+        
+        for param in self.d_features.parameters():
+            param.requires_grad = True
+
+
+class AlexnetMap_v3(nn.Module):
+    def __init__(self):
+        super(AlexnetMap_v3, self).__init__()
+        pretrained_alexnet = alexnet(pretrained=True)
+        self.rgb_features = pretrained_alexnet.features[:6]
+        self.d_features = pretrained_alexnet.features[:6]
+        self.features = nn.Sequential(
+            nn.Conv2d(192+192, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            
+            nn.ConvTranspose2d(128, 64, kernel_size=3, stride=2),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 64, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(64, 32, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.ReLU(inplace=True)
+        )
+        self.grasp = nn.Sequential(
+            nn.ConvTranspose2d(32, 5, kernel_size=11, stride=4, output_padding=1),
             nn.Tanh()
+        )
+
+        self.confidence = nn.Sequential(
+            nn.ConvTranspose2d(32, 1, kernel_size=11, stride=4, output_padding=1),
+            nn.Sigmoid()
+        )
+
+        for param in self.rgb_features.parameters():
+            param.requires_grad = False
+        for param in self.d_features.parameters():
+            param.requires_grad = False
+
+        # xavier initialization for combined feature extractor
+        for m in self.features.modules():
+            if isinstance(m, (nn.Conv2d, nn.ConvTranspose2d, nn.Linear)):
+                nn.init.xavier_uniform_(m.weight, gain=1)
+
+    def forward(self, x):
+        rgb = x[:, :3, :, :]
+        d = torch.unsqueeze(x[:, 3, :, :], dim=1)
+        d = torch.cat((d, d, d), dim=1)
+
+        rgb = self.rgb_features(rgb)
+        d = self.d_features(d)
+        x = torch.cat((rgb, d), dim=1)
+
+        x = self.features(x)
+        grasp = self.grasp(x)
+        confidence = self.confidence(x)
+        out = torch.cat((grasp, confidence), dim=1)
+
+        return out
+
+    # Unfreeze pretrained layers (1st & 2nd CNN layer)
+    def unfreeze_depth_backbone(self):
+        for param in self.rgb_features.parameters():
+            param.requires_grad = True
+        
+        for param in self.d_features.parameters():
+            param.requires_grad = True
+
+
+class AlexnetMap_v4(nn.Module):
+    def __init__(self):
+        super(AlexnetMap_v4, self).__init__()
+        pretrained_alexnet = alexnet(pretrained=True)
+        self.rgb_features = pretrained_alexnet.features[:6]
+        self.d_features = pretrained_alexnet.features[:6]
+        self.features = nn.Sequential(
+            nn.Conv2d(192+192, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.2),
+
+            ResidualBlock(128, 128, 3),
+
+            nn.Conv2d(128, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.2),
+            
+            ResidualBlock(128, 128, 3),
+            
+            nn.ConvTranspose2d(128, 64, kernel_size=3, stride=2),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 64, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(64, 32, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.ReLU(inplace=True)
+        )
+        self.grasp = nn.Sequential(
+            nn.ConvTranspose2d(32, 5, kernel_size=11, stride=4, output_padding=1),
+            nn.Tanh()
+        )
+
+        self.confidence = nn.Sequential(
+            nn.ConvTranspose2d(32, 1, kernel_size=11, stride=4, output_padding=1),
+            nn.Sigmoid()
         )
 
         for param in self.rgb_features.parameters():
